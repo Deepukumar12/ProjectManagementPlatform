494178b19601ceb96f008a04fc2a4c09f6f721af
DeepuKumar <deeputhakur0986@gmail.com>
2026-07-01 01:02:55 +0530

    feat: add dotenv for environment variable management and nodemon for development - Integrated dotenv to load environment variables from a .env file - Added nodemon as a development dependency for automatic server restarts - Updated package.json scripts to include a dev command for running the server with nodemon - Updated package-lock.json and package.json to reflect new dependencies

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+
+let myusername = process.env.username;
+console.log("value = " ,myusername);
+
+let mydatabase = process.env.database;
+console.log("database = " , mydatabase);
+
 console.log("starting a backend project!");
+console.log("starting a project management platform!");
