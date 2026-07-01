33a5d52792e7af2232cf2c89c6d8af2661577348
DeepuKumar <deeputhakur0986@gmail.com>
2026-07-02 05:19:05 +0530

    feat: add ApiError and ApiResponse classes for standardized error and response handling

---
+++ src/utils/api-error.js
@@ -0,0 +1,20 @@
+class ApiError extends Error{
+    constructor(statusCode, message = "Something went wrong", errors = [], stack = ""){
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.errors = errors;
+        this.success = false;
+        this.stack = stack;
+
+        if(stack){
+            this.stack = stack;
+        }else{
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+};
+
+export {
+    ApiError
+};
