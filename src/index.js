f4bbdea8a48e2b448918e5da2c830a3fb8779a88
DeepuKumar <deeputhakur0986@gmail.com>
2026-07-02 00:39:43 +0530

    feat: add express dependency and set up basic server with routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config({
     path: "./.env"
 });
 
-let myusername = process.env.username;
-console.log("value = " ,myusername);
+// let myusername = process.env.username;
+// console.log("value = " ,myusername);
 
-let mydatabase = process.env.database;
-console.log("database = " , mydatabase);
+// let mydatabase = process.env.database;
+// console.log("database = " , mydatabase);
 
-console.log("starting a backend project!");
-console.log("starting a project management platform!");
+// console.log("starting a backend project!");
+// console.log("starting a project management platform!");
+
+
+// const express = require('express');
+const app = express() 
+const port = process.env.PORT  || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/instagram", (req,res) => {
+    res.send("This is an instagram page")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
