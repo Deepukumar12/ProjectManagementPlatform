d229b617287390640a32f11536bb4155216ceec4
DeepuKumar <deeputhakur0986@gmail.com>
2026-07-02 03:32:58 +0530

    feat: add CORS support and update package dependencies

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
@@ -14,6 +15,19 @@ app.get("/google", (req, res) => {
     res.send("This is a google page");
 });
 
+// Middleware-> basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb"}));
+app.use(express.static("public"));
+
+// CORS configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
+}),
+);
 
 
 
