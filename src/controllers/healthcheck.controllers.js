e8883f82bef8b400697182d3da06b78e9782f4ae
DeepuKumar <deeputhakur0986@gmail.com>
2026-07-21 02:14:52 +0530

    feat: implement asyncHandler for improved error handling in healthcheck controller

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,14 +1,23 @@
 import {ApiResponse} from "../utils/api-response.js";
+import {asyncHandler} from "../utils/async-handler.js";
 
-const healthcheck = (req, res) => {
-    try{
-        res.status(200).json(
-        new ApiResponse(200, {message: "Server is runnning"}));
-    }
-    catch(error){
+// const healthcheck = async (req, res, next) => {
+//     try{
+//         const user = await getUserFromDB();
+//         res
+//         .status(200)
+//         .json(new ApiResponse(200, {message: "Server is runnning"}));
+//     }
+//     catch(error){
+//         next(error);
+//     }
+// };
 
-    }
-};
+const healthcheck = asyncHandler(async (req, res) => {
+    res
+    .status(200)
+    .json(new ApiResponse(200, {message: "Server is running"}));
+});
 
 
 export { healthcheck };
