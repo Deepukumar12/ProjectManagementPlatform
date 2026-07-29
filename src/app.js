4ee2bbe7941d5a7465fc5cf8aa19e74316b9bfd1
DeepuKumar <deeputhakur0986@gmail.com>
2026-07-29 19:28:54 +0530

    feat: add cookie-parser middleware and implement login functionality with token generation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.get("/google", (req, res) => {
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb"}));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 // CORS configuration
 app.use(cors({
